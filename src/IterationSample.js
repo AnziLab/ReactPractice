ea8bd5b46000f359b59eb7d37bad4f3f93c0c585
Jihoon from School <gorgeouskid@naver.com>
2021-03-26 16:27:13 +0900

    p.225 app component

---
+++ src/IterationSample.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IterationSample =()=>{
+    return(
+        <ul>
+            <li>snow man</li>
+            <li>ice</li>
+            <li>snow</li>
+            <li>wind</li>
+        </ul>
+    );
+};
+
+export default IterationSample;
