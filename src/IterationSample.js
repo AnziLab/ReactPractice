36bad6a25d64bda96f534909cadc87e396c90bf1
Jihoon from School <gorgeouskid@naver.com>
2021-03-29 13:27:14 +0900

    p.255

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const IterationSample =()=>{
-    return(
-        <ul>
-            <li>snow man</li>
-            <li>ice</li>
-            <li>snow</li>
-            <li>wind</li>
-        </ul>
+const IterationSample = () => {
+    const [names, setNames] = useState([
+        { id: 1, text: 'snowman' },
+        { id: 2, text: 'ice' },
+        { id: 3, text: 'snow' },
+        { id: 4, text: 'wind' }
+    ]);
+
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = (e)=> setInputText(e.target.value);
+    const onClick =()=>{
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId +1);
+        setNames(nextNames);
+        setInputText('');
+    };
+
+    const onKeyPress=e=>{
+        if(e.key==='Enter'){
+            onClick();
+        }
+    }
+    const onRemove =e=>{
+        const nextNames = names.filter(name=> name.id < e);
+        setNames(nextNames);
+    }
+    const nameList = names.map(name => <li key={name.id} onDoubleClick={()=> onRemove(name.id)}>{name.text}, {name.id}</li>);
+    return (
+        <>
+            <input value={inputText} onKeyPress={onKeyPress} onChange={onChange} />
+            <button onclick={onclick}> add </button>
+            <ul> {nameList} </ul>
+        </>
     );
 };
 
