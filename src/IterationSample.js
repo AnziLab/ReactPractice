0d3b604bce825a358f9dc090b7b1354e7059a215
Jihoon from School <gorgeouskid@naver.com>
2021-03-29 16:14:36 +0900

    7장 완료

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -11,31 +11,31 @@ const IterationSample = () => {
     const [inputText, setInputText] = useState('');
     const [nextId, setNextId] = useState(5);
 
-    const onChange = (e)=> setInputText(e.target.value);
-    const onClick =()=>{
+    const onChange = (e) => setInputText(e.target.value);
+    const onClick = () => {
         const nextNames = names.concat({
             id: nextId,
             text: inputText
         });
-        setNextId(nextId +1);
+        setNextId(nextId + 1);
         setNames(nextNames);
         setInputText('');
     };
 
-    const onKeyPress=e=>{
-        if(e.key==='Enter'){
+    const onKeyPress = e => {
+        if (e.key === 'Enter') {
             onClick();
         }
     }
-    const onRemove =e=>{
-        const nextNames = names.filter(name=> name.id < e);
+    const onRemove = e => {
+        const nextNames = names.filter(name => name.id < e);
         setNames(nextNames);
     }
-    const nameList = names.map(name => <li key={name.id} onDoubleClick={()=> onRemove(name.id)}>{name.text}, {name.id}</li>);
+    const nameList = names.map(name => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}, {name.id}</li>);
     return (
         <>
             <input value={inputText} onKeyPress={onKeyPress} onChange={onChange} />
-            <button onclick={onclick}> add </button>
+            <button onclick={onClick}> add </button>
             <ul> {nameList} </ul>
         </>
     );
